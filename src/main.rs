b9652c883202a56abd5941b5868191f0bc3082eb
Wink Saville <wink@saville.com>
2022-02-15 12:33:13 -0800

    Add PartialEq

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,27 @@ impl ExprEqu {
         }
     }
 }
-fn main() {
-    let eet = ExprEqu::new();
 
-    println!("eet: {eet:?}!");
+// Don't use all of the fields
+impl PartialEq for ExprEqu {
+    fn eq(&self, other: &Self) -> bool {
+        self.s == other.s
+    }
+}
+
+fn main() {
+    let mut eet1 = ExprEqu::new();
+    let mut eet2 = ExprEqu::new();
+
+    eet1.n = 1;
+    eet1.s = "1".to_owned();
+
+    eet2.n = 2;
+    eet2.s = "2".to_owned();
+
+    println!("eet1: {eet1:?}!");
+    println!("eet2: {eet2:?}!");
+    assert_eq!(eet1, eet1);
+    assert_ne!(eet1, eet2);
+    assert_ne!(eet2, eet1);
 }
