a6e7377b2cf46ebb0173e95baddf7095c6e8ed79
Wink Saville <wink@saville.com>
2022-02-15 12:26:21 -0800

    Add imp ExprEqu new

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+#[derive(Debug)]
+pub struct ExprEqu {
+    pub n: i64,
+    pub s: String,
+}
+
+impl ExprEqu {
+    fn new() -> ExprEqu {
+        ExprEqu {
+            n: 0,
+            s: "".to_owned(),
+        }
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let eet = ExprEqu::new();
+
+    println!("eet: {eet:?}!");
 }
